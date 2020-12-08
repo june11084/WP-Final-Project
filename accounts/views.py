9b0d84b469a4ec4ebe2c0963a49f106b34ae3852
Aidan <wardmantheyal@my.ccsu.edu>
2020-12-08 17:20:54 -0500

    Login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def index(request):
+    return render(request, 'accounts/index.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
