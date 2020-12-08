93d15653d6ae084661c77945d7afb42681b949e1
Jun Li <june11084@gmail.com>
2020-12-08 13:09:04 -0500

    add accounts module

--- ToDo_Project/settings.py
+++ ToDo_Project/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ToDo.apps.TodoConfig'
-    'accounts'
+    'ToDo.apps.TodoConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
