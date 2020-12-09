9718745887e34b0d36b0f5fde434dd774ba50ea2
Aidan <wardmantheyal@my.ccsu.edu>
2020-12-08 19:11:06 -0500

    Login and Register

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+@login_required(login_url='login')
 def index(request):
     return render(request, 'accounts/index.html')
 
@@ -19,3 +22,20 @@ def register_view(request):
     return render(request, 'accounts/register.html', context)
 
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
